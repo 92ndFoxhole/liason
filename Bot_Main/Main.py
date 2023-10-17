c8646bf27865438e43b7a8e54fb389780c329415
casualpigeon <105026911+casualpigeon@users.noreply.github.com>
2023-10-17 16:31:49 -0600

    Update Main.py

--- Bot_Main/Main.py
+++ Bot_Main/Main.py
@@ -1,7 +1,24 @@
 import discord
 
+TOKEN = "MTE2MzU2NDIzMjU1NDU5NDQzNw.G-ew3i.U_KaH3NtCY0SQ0fITX6JVDntp7dgTYebJjXBSc"
+
+
+
 def main():
-    pass
+    
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = discord.Client(intents=intents)
+    
+    @client.event
+
+    async def on_ready():
+        
+        print(f'We have logged in as {client.user}')
+
+        
+    client.run(TOKEN)
 
 
 
