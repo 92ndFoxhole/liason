a34f8689b5ed721b8f5cca3ff127885b70b1ebc9
casualpigeon <105026911+casualpigeon@users.noreply.github.com>
2023-11-16 19:58:32 -0700

    Added cogs and fixed calling .gets on command
    Ok so main calls the cog verify/deny verify/deny contains all of the commands rn but for organization we can add other commands probably 1 cog per issue on git.

--- Bot_Main/Main.py
+++ Bot_Main/Main.py
@@ -1,51 +1,12 @@
-
 import discord
 from discord.ext import commands
 
 
-description = """The 92nd regiment bot"""
-
-intents = discord.Intents.default()
-intents.members = True
 
 
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("!"),
-    description=description,
-    intents=intents,
-)
-    
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
-    print("------")
-    
-    
-@bot.command(description='Deny members to the 92nd regiment takes in their user id, or their name, or just ping them also has an optional field for reason which will appear in audit logs default reason is "verified" example: !verify casualpigeon "Hes the dev writing this"')
-async def deny(ctx, member: discord.Member, giveReason = "Denied"):
-    """This system of calling all the channels/roles  by id in this command is terrible but is simply for testing purposes I will fix this tommorow if I am free"""
-    guild = bot.get_guild(1163561993530253375)      
-    recruit = guild.get_role(1174547968796393494)
-    verifcationChannel = guild.get_channel(1174552689280045106)
-    await member.remove_roles(recruit, reason=giveReason)
-    await verifcationChannel.send(member.display_name + " Has been denied reason: " + giveReason)
-        
-
-@bot.command(description='Verify members to the 92nd regiment takes in their user id, or their name, or just ping them also has an optional field for reason which will appear in audit logs default reason is "verified" example: !verify casualpigeon "Hes the dev writing this"')
-async def verify(ctx, member: discord.Member, giveReason = "Verified"):
-    """This system of calling all the channels/roles  by id in this command is terrible but is simply for testing purposes I will fix this tommorow if I am free"""
-    guild = bot.get_guild(1163561993530253375)      
-    cadet_role = guild.get_role(1174541862204342282)
-    member_role = guild.get_role(1174535263578497125)
-    recruit = guild.get_role(1174547968796393494)
-    discussion = guild.get_channel(1174553795699671151)
-    verifcationChannel = guild.get_channel(1174552689280045106)
-    await member.add_roles(member_role, cadet_role, reason=giveReason)
-    await member.remove_roles(recruit, reason=giveReason)
-    await discussion.send(member.mention + "welcome to the 92nd, check out #role-assignment and ask if you have any questions.")
-    await verifcationChannel.send(member.display_name + " Has been verified reason: " + giveReason)
-    
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
+bot.load_extension("VerifyDeny_Cog")
 bot.run("TOKEN")
 
 
---
+++ Bot_Main/VerifyDeny_Cog.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+
+
+class BotCommands(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+        
+    def idPusher(self):
+        self.guild = self.bot.get_guild(1163561993530253375)      
+        self.cadet_role = self.guild.get_role(1174541862204342282)
+        self.member_role = self.guild.get_role(1174535263578497125)
+        self.recruit = self.guild.get_role(1174547968796393494)
+        self.discussion = self.guild.get_channel(1174553795699671151)
+        self.verifcationChannel = self.guild.get_channel(1174552689280045106)
+        self.roleAssignment = self.guild.get_channel(1174559644115546232)
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Logged in as {self.bot.user} (ID: {self.bot.user.id})")
+        print("------")
+        self.idPusher() #Note here that idpusher is not called in init because to use get_role/chennel you have to connect to discord servers first.
+    @commands.command()
+    async def deny(self, ctx, member: discord.Member, giveReason = "Denied"):
+        
+        
+        await member.remove_roles(self.recruit, reason=giveReason)
+        await self.verifcationChannel.send(member.display_name + " Has been denied reason: " + giveReason)
+            
+
+    @commands.command()
+    async def verify(self, ctx, member: discord.Member, giveReason = "Verified"):
+        
+        
+        await member.add_roles(self.member_role, self.cadet_role, reason=giveReason)
+        await member.remove_roles(self.recruit, reason=giveReason)
+        await self.discussion.send(member.mention + " Welcome to the 92nd, check out " + self.roleAssignment.mention + " and ask if you have any questions.")
+        await self.verifcationChannel.send(member.display_name + " Has been verified reason: " + giveReason)
+
+def setup(bot):
+    bot.add_cog(BotCommands(bot))
