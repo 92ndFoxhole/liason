3baced06808cd845fd9de78b9d8c0c63c2632b72
casualpigeon <105026911+casualpigeon@users.noreply.github.com>
2023-11-18 13:42:51 -0700

    Added the member activity system.
    Not finished but should be enough to work with for the sql part.

--- Bot_Main/Activity_Cog.py
+++ Bot_Main/Activity_Cog.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.all()
 
 class BotCommands(commands.Cog):
     
@@ -10,13 +11,55 @@ class BotCommands(commands.Cog):
     def idPusher(self):
         self.guild = self.bot.get_guild(1163561993530253375)  
         self.recruit = self.guild.get_role(1174547968796393494)
-
+        self.activityChannel = self.guild.get_channel(1175517710587809792) 
+        self.inactiveRole = self.guild.get_role(1175517567641727046)
+        self.message_id = None
     
-    @commands.has_permissions(manage_roles=True)
+    
+    
     @commands.Cog.listener()
     async def on_ready(self):
         self.idPusher()
+
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if self.message_id == None:
+            # Reaction message not sent yet
+            return
         
+        
+        #make sure it is the react message
+        if payload.message_id != self.message_id:
+            return
+
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if is in guild
+            return
+
+        try:
+            # Reset active wars on database for member
+            await payload.member.remove_roles(self.recruit, "Verified active")
+            pass # Use await here then add whatever the function for doing that is here
+            
+        except discord.HTTPException:
+            
+            raise "Failed to updated users roles"
+   
+   
+    @commands.is_owner()
+    @commands.command()
+    async def newWar(self, ctx):
+        #increase inactive wars by 1
+        message = await self.activityChannel.send("Activity message, react to be marked active for this war.")
+        self.message_id = message.id
+        print()
+        for member in self.guild.members:
+            await member.add_roles(self.inactiveRole) #Should work but for some reason guild.members only returns the bot. WIP
+        
+        
+    
     @commands.has_permissions(manage_roles=True)
     @commands.command()
     async def active(self, ctx, member: discord.Member, giveReason = "No reason given"):
@@ -41,8 +84,8 @@ class BotCommands(commands.Cog):
     async def leaveOfAbsence(self, ctx, member: discord.Member, giveReason = "No reason given"):
         pass 
     
-    @commands.is_owner()
     @commands.command()
+    @commands.is_owner()
     async def massDemotion(self, ctx, inactiveWars):
         
         if inactiveWars != int:
